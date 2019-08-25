b81c2676a1cd3a579dc1617bb3639813c59645c9
Varlin Sheffey <varlin@varlins-macbook-pro.local>
2019-08-24 20:53:17 -0500

    Initial Commit

---
+++ InnovationLab Sign-In/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InnovationLab Sign-In
+//
+//  Created by Varlin Sheffey on 8/24/19.
+//  Copyright © 2019 Varlin Sheffey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ InnovationLab Sign-InTests/InnovationLab_Sign_InTests.swift
@@ -0,0 +1,34 @@
+//
+//  InnovationLab_Sign_InTests.swift
+//  InnovationLab Sign-InTests
+//
+//  Created by Varlin Sheffey on 8/24/19.
+//  Copyright © 2019 Varlin Sheffey. All rights reserved.
+//
+
+import XCTest
+@testable import InnovationLab_Sign_In
+
+class InnovationLab_Sign_InTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
